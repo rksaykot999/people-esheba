3a73696f6cbc032aa0d45be8e6527b9ec9635363
RK Saykot <rksaikatkhan999@gmail.com>
2026-07-07 20:33:35 +0600

    server.js file updated

--- server/src/server.js
+++ server/src/server.js
@@ -59,7 +59,10 @@ app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 
 // ── Logging ───────────────────────────────────────────────────
-if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+};
 
 // ── Static Uploads ────────────────────────────────────────────
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
